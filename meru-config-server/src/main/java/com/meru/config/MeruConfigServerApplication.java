e0ba90e2d82e1b8a336d4e89c4e354a8421e5107
Rajesh Kothari <rajeshkotharirk@gmail.com>
2020-10-31 00:01:01 +0530

    Config Server Structure added

---
+++ meru-config-server/src/main/java/com/meru/config/MeruConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.meru.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeruConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeruConfigServerApplication.class, args);
+	}
+
+}
---
+++ meru-config-server/src/test/java/com/meru/config/MeruConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meru.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeruConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
